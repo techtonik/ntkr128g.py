f003ba05d1422490c214db13e8bd0bbfe69f8ce1
anatoly techtonik <techtonik@gmail.com>
2014-12-04 16:15:28 +0300

    Check that patched version exists first before size check

--- ntkr128g.py
+++ ntkr128g.py
@@ -14,8 +14,22 @@
 # [ ] report
 
 
+import os
+import sys
+import shutil
+
 import ctypes    # using Windows API directly
 import ctypes.wintypes
+
+# signature to find
+signature1 = '7C 11 8B 45 FC 85 C0 74 0A'  # test for failure
+signature2 = '7C 10 8B 45 FC 85 C0 74 09'  # test for license
+
+replacemnt =       'B8 00 00 02 00 90 90'  # patch
+
+
+needsaction = 0
+
 
 def system32(name):
   """return unicode path to filename in system32 dir"""
@@ -27,19 +41,30 @@ def system32(name):
 
 original = system32('ntkrnlpa.exe')
 patched = system32('ntkr128g.exe')
+local = 'ntkr128g.exe'
+
+def copyntkr():
+  pass # [ ]
+
+print('[*] Checking if patch is present')
+if not os.path.exists(patched):
+  # [ ] also check that OS is eligible
+  print('    ntkr128g.exe is not found..')
+  print('[*] Copying ntkrnlpa.exe to ntkr128g.exe for patching..')
+  shutil.copy(original, local)
+  needsaction += 1
+else:
+  print('[*] Checking sizes')
+  print('      ntkrnlpa.exe  %s' % os.path.getsize(original))
+  print('      ntkr128g.exe  %s' % os.path.getsize(patched))
+  if os.path.getsize(original) != os.path.getsize(patched):
+    print('    Kernel 128Gb patch needs to be updated ')
+    needsaction += 1
+  print('[*] Copying ntkrnlpa.exe to ntkr128g.exe for patching..')
+  shutil.copy(original, local)
+
+#print('[*] Checking signature in ntkr128g.exe')
+# [ ]
 
 
-warnings = 0
-
-import os
-
-print('[*] Checking sizes')
-print('      ntkrnlpa.exe  %s' % os.path.getsize(original))
-print('      ntkr128g.exe  %s' % os.path.getsize(patched))
-if os.path.getsize(original) != os.path.getsize(patched):
-  print('WARNING: kernel 128Gb patch needs upgrading')
-  warnings += 1
-
-
-import sys
-sys.exit(warnings)
+sys.exit(needsaction)
